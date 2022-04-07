8f1b043d1ff5aa1ba5ab8a0096bda196c3224e71
tchiteu <matheussantos@estudante.sc.senai.br>
2022-04-06 23:43:55 -0300

    Initializing the project with expo bare workflow :tada:

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -10,6 +10,8 @@ export default function App() {
     </View>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
